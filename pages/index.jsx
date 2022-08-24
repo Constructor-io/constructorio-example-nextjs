cf4c0bcac652c3b2f66fa94e7da205e22298790a
Steve Blaurock <sblaurock@gmail.com>
2022-08-24 13:45:01 -0600

    Update content, add links to client libraries.

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,38 +3,54 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="mx-auto text-center max-w-[2000px] pt-4">
-      <h1 className="my-8 text-2xl font-bold">Constructor.io Next.js Example</h1>
+      <h1 className="my-4 text-2xl font-bold">Constructor.io Next.js Example</h1>
       <p>
-        This is a repository that showcases different options to integrate Constructor.io SDKs into a Next.js project.
+        This repository showcases different options to integrate Constructor.io client libraries into a Next.js project.
       </p>
       <div className="flex justify-around mt-12">
-        <div className="flex flex-col justify-between border border-2 border-gray-500 rounded-lg p-4 w-[30%]">
-          <h3 className="text-lg font-bold">Client Side Rendering</h3>
-          <p className="my-4">
-            This is the recommended option due to implementation simplicity and to prevent installing two different packages unless there is a specific need to do SSR.
-            In this option, the requests are made directly from the end user&apos;s browser to Constructor.io servers so the end to end response time is faster.
+        <div className="flex flex-col justify-between border border-2 border-gray-300 p-4 w-[30%]">
+          <h3 className="text-lg font-bold mb-2">Client Side Rendering</h3>
+          <p className="my-2">
+            <strong>Recommended</strong> - This option is the simplest to implement, yields the fastest response times, and avoids the need to install multiple client libraries.
+          </p>
+          <p className="my-2">
+            In this option, requests are made directly from the end user&apos;s browser to Constructor.io servers. As a result, the end to end response time is fastest.
+          </p>
+          <p className="my-2">
+            <a class="text-blue-600" href="https://www.npmjs.com/package/@constructor-io/constructorio-client-javascript" rel="nofollow" target="_blank">constructorio-client-javascript</a>
           </p>
           <Link href="/client-side">
-            <a className="block border border-gray-500 mt-4 rounded-lg px-4 py-2">Example</a>
+            <a className="block border border-gray-500 mt-4 px-4 py-2">Example</a>
           </Link>
         </div>
-        <div className="flex flex-col justify-between border border-2 border-gray-500 rounded-lg p-4 w-[30%]">
-          <h3 className="text-lg font-bold">Server Side Rendering</h3>
-          <p className="my-4">
-            This option uses server side rendering for all of the requests. It only uses the server side Constructor.io package (constructorio-node). The data is fetched on the server and passed to the
-            client by Next.js. Next Router can be used to refresh props (fetch new results) after user interacts with the page.
+        <div className="flex flex-col justify-between border border-2 border-gray-300 p-4 w-[30%]">
+          <h3 className="text-lg font-bold mb-2">Server Side Rendering</h3>
+          <p className="my-2">
+            This option uses server-side rendering utilizing only the server side client library.
+          </p>
+          <p className="my-2">
+            The data is fetched on the server and passed to the client through Next.js. Next Router can be used to refresh props (fetch new results) after user interacts with the page.
+          </p>
+          <p className="my-2">
+            <a class="text-blue-600" href="https://www.npmjs.com/package/@constructor-io/constructorio-node" rel="nofollow" target="_blank">constructorio-node</a>
           </p>
           <Link href="/server-side">
-            <a className="block border border-gray-500 mt-4 rounded-lg px-4 py-2">Example</a>
+            <a className="block border border-gray-500 mt-4 px-4 py-2">Example</a>
           </Link>
         </div>
-        <div className="flex flex-col justify-between border border-2 border-gray-500 rounded-lg p-4 w-[30%]">
-          <h3 className="text-lg font-bold">Client + Server Side Rendering</h3>
-          <p className="my-4">
-            This option uses server side rendering for the initial page load and uses client side rendering for the subsequent requests. It uses appropriate Constructor.io package depending on the context.
+        <div className="flex flex-col justify-between border border-2 border-gray-300 p-4 w-[30%]">
+          <h3 className="text-lg font-bold mb-2">Client + Server Side Rendering</h3>
+          <p className="my-2">
+            This option uses server side rendering for the initial page load and client side rendering for the subsequent requests.
+          </p>
+          <p className="my-2">
+            It utilizes the appropriate Constructor.io client library depending on the context.
+          </p>
+          <p className="my-2">
+            <a class="text-blue-600" href="https://www.npmjs.com/package/@constructor-io/constructorio-client-javascript" rel="nofollow" target="_blank">constructorio-client-javascript</a><br /><a class="text-blue-600" href="https://www.npmjs.com/package/@constructor-io/constructorio-node" rel="nofollow" target="_blank">constructorio-node</a>
           </p>
           <Link href="/client-and-server-side">
-            <a className="block border border-gray-500 mt-4 rounded-lg px-4 py-2">Example</a>
+            <a className="block border border-gray-500 mt-4 px-4 py-2">Example</a>
           </Link>
         </div>
       </div>
