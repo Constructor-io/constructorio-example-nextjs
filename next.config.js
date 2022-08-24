8a904ab223121482147c5c844cb501897f07d704
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2022-08-24 14:08:44 -0400

    Add image domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [ "constructorio-integrations.s3.amazonaws.com" ]
+  }
 }
 
 module.exports = nextConfig
