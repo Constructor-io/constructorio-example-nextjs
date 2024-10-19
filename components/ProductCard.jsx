05e31e5a30332fa3fa6748cd249345a82d65e29e
Hyukoo Kwon <hyuk.kwon.epic@outlook.com>
2024-10-19 19:32:09 +0900

    Update to nextjs 14
    - Bumped up dependencies (https://nextjs.org/docs/pages/building-your-application/upgrading/version-13, https://nextjs.org/docs/pages/building-your-application/upgrading/version-14)
    - Changed Link component not to wrap anchor element (https://nextjs.org/docs/pages/api-reference/components/link#legacybehavior)
    - Changed Image component's width and height to be number

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,26 +1,31 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 function ProductCard({ product }) {
-  const imageTagClassesLoading = 'transition-opacity opacity-0 ml-auto mr-auto';
-  const imageTagClassesLoaded = 'transition-opacity opacity-100 ml-auto mr-auto';
+  const imageTagClassesLoading = "transition-opacity opacity-0 ml-auto mr-auto";
+  const imageTagClassesLoaded =
+    "transition-opacity opacity-100 ml-auto mr-auto";
 
   return (
     <div
       className="product-card text-center"
-      data-cnstrc-item-id={ product.data.id }
-      data-cnstrc-item-name={ product.value }
-      data-cnstrc-item-variation-id={ product.data?.variation_id }
+      data-cnstrc-item-id={product.data.id}
+      data-cnstrc-item-name={product.value}
+      data-cnstrc-item-variation-id={product.data?.variation_id}
     >
       <div className="mb-1 h-[225px]">
         <Image
-          className={ imageTagClassesLoading }
-          src={ product.data.image_url }
-          alt={ product.value }
-          onError={ (event) => { event.target.style.display = 'none'; } }
-          onLoad={ (event) => { event.target.className = imageTagClassesLoaded; } }
-          width="225"
-          height="225"
-          />
+          className={imageTagClassesLoading}
+          src={product.data.image_url}
+          alt={product.value}
+          onError={(event) => {
+            event.target.style.display = "none";
+          }}
+          onLoad={(event) => {
+            event.target.className = imageTagClassesLoaded;
+          }}
+          width={225}
+          height={225}
+        />
       </div>
       {product.value}
     </div>
@@ -28,4 +33,3 @@ function ProductCard({ product }) {
 }
 
 export default ProductCard;
-
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,61 +1,112 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="mx-auto text-center max-w-[2000px] pt-4">
-      <h1 className="my-4 text-2xl font-bold">Constructor.io Next.js Example</h1>
+      <h1 className="my-4 text-2xl font-bold">
+        Constructor.io Next.js Example
+      </h1>
       <p>
-        This repository showcases different options to integrate Constructor.io client libraries into a Next.js project.
+        This repository showcases different options to integrate Constructor.io
+        client libraries into a Next.js project.
       </p>
       <div className="flex justify-around mt-10">
         <div className="flex flex-col rounded-lg justify-between border border-2 border-gray-300 p-4 w-[30%]">
           <h3 className="text-lg font-bold mb-2">Client Side Rendering</h3>
           <p className="my-2">
-            <strong>Recommended</strong> - This option is the simplest to implement, yields the fastest response times, and avoids the need to install multiple client libraries.
+            <strong>Recommended</strong> - This option is the simplest to
+            implement, yields the fastest response times, and avoids the need to
+            install multiple client libraries.
           </p>
           <p className="my-2">
-            In this option, requests are made directly from the end user&apos;s browser to Constructor.io servers. As a result, the end to end response time is fastest.
+            In this option, requests are made directly from the end user&apos;s
+            browser to Constructor.io servers. As a result, the end to end
+            response time is fastest.
           </p>
           <p className="my-2">
-            <a className="text-blue-600" href="https://www.npmjs.com/package/@constructor-io/constructorio-client-javascript" rel="noreferrer" target="_blank">constructorio-client-javascript</a>
+            <a
+              className="text-blue-600"
+              href="https://www.npmjs.com/package/@constructor-io/constructorio-client-javascript"
+              rel="noreferrer"
+              target="_blank"
+            >
+              constructorio-client-javascript
+            </a>
           </p>
-          <Link legacyBehavior href="/client-side">
-            <a className="block mt-4 rounded-lg px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white font-semibold tracking-wide">Example</a>
+          <Link
+            href="/client-side"
+            className="block mt-4 rounded-lg px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white font-semibold tracking-wide"
+          >
+            Example
           </Link>
         </div>
         <div className="flex flex-col rounded-lg justify-between border border-2 border-gray-300 p-4 w-[30%]">
           <h3 className="text-lg font-bold mb-2">Server Side Rendering</h3>
           <p className="my-2">
-            This option uses server-side rendering utilizing only the server side client library.
+            This option uses server-side rendering utilizing only the server
+            side client library.
           </p>
           <p className="my-2">
-            The data is fetched on the server and passed to the client through Next.js. Next Router can be used to refresh props (fetch new results) after user interacts with the page.
+            The data is fetched on the server and passed to the client through
+            Next.js. Next Router can be used to refresh props (fetch new
+            results) after user interacts with the page.
           </p>
           <p className="my-2">
-            <a className="text-blue-600" href="https://www.npmjs.com/package/@constructor-io/constructorio-node" rel="noreferrer" target="_blank">constructorio-node</a>
+            <a
+              className="text-blue-600"
+              href="https://www.npmjs.com/package/@constructor-io/constructorio-node"
+              rel="noreferrer"
+              target="_blank"
+            >
+              constructorio-node
+            </a>
           </p>
-          <Link legacyBehavior href="/server-side">
-            <a className="block mt-4 rounded-lg px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white font-semibold tracking-wide">Example</a>
+          <Link
+            href="/server-side"
+            className="block mt-4 rounded-lg px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white font-semibold tracking-wide"
+          >
+            Example
           </Link>
         </div>
         <div className="flex flex-col rounded-lg justify-between border border-2 border-gray-300 rounded-lg p-4 w-[30%]">
-          <h3 className="text-lg font-bold mb-2">Client + Server Side Rendering</h3>
+          <h3 className="text-lg font-bold mb-2">
+            Client + Server Side Rendering
+          </h3>
           <p className="my-2">
-            This option uses server side rendering for the initial page load and client side rendering for the subsequent requests.
+            This option uses server side rendering for the initial page load and
+            client side rendering for the subsequent requests.
           </p>
           <p className="my-2">
-            It utilizes the appropriate Constructor.io client library depending on the context.
+            It utilizes the appropriate Constructor.io client library depending
+            on the context.
           </p>
           <p className="my-2">
-            <a className="text-blue-600" href="https://www.npmjs.com/package/@constructor-io/constructorio-client-javascript" rel="noreferrer" target="_blank">constructorio-client-javascript</a>
+            <a
+              className="text-blue-600"
+              href="https://www.npmjs.com/package/@constructor-io/constructorio-client-javascript"
+              rel="noreferrer"
+              target="_blank"
+            >
+              constructorio-client-javascript
+            </a>
             <br />
-            <a className="text-blue-600" href="https://www.npmjs.com/package/@constructor-io/constructorio-node" rel="noreferrer" target="_blank">constructorio-node</a>
+            <a
+              className="text-blue-600"
+              href="https://www.npmjs.com/package/@constructor-io/constructorio-node"
+              rel="noreferrer"
+              target="_blank"
+            >
+              constructorio-node
+            </a>
           </p>
-          <Link legacyBehavior href="/client-and-server-side">
-            <a className="block mt-4 rounded-lg px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white font-semibold tracking-wide">Example</a>
+          <Link
+            href="/client-and-server-side"
+            className="block mt-4 rounded-lg px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white font-semibold tracking-wide"
+          >
+            Example
           </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
